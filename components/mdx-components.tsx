7ab8bae168a878b98ac28a293fbc625f0d2575f9
a-aigner <andre.onephone@gmail.com>
2026-06-11 12:58:29 +0200

    style fix

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -6,6 +6,6 @@ export const mdxComponents: MDXComponents = {
   p: (props) => <p className="mt-4 leading-relaxed text-[rgb(10_10_10/0.78)]" {...props} />,
   ul: (props) => <ul className="mt-4 list-disc space-y-1 pl-6" {...props} />,
   a: (props) => <a className="underline underline-offset-2 hover:opacity-70" {...props} />,
-  pre: (props) => <pre className="mt-4 overflow-x-auto rounded-card-sm bg-[#0a0a0a] p-4 text-sm" {...props} />,
+  pre: (props) => <pre className="mt-4 overflow-x-auto rounded-card-sm bg-[#0a0a0a] p-4 text-sm text-[#e6edf3]" {...props} />,
   code: (props) => <code className="rounded bg-[rgb(10_10_10/0.06)] px-1 py-0.5 text-sm" {...props} />,
 };
